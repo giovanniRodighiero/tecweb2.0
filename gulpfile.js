3076c657bafd46f97ef84efc6b8f0d8d9b34af28
Giovanni Rodighiero <giova.rod93@gmail.com>
2016-07-22 21:12:45 +0200

    started first experiments on anagrafica

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 var pug = require('gulp-pug');
 var gulp = require('gulp');
 
-gulp.task('views', function buildHTML() {
-  return gulp.src('assets/**/*.pug')
+gulp.task('xml', function buildHTML() {
+  return gulp.src('assets/xml/**/*.pug')
   .pipe(pug({
     pretty: true
     // Your options in here.
   }))
-  .pipe(gulp.dest('public_html/'))
+  .pipe(gulp.dest('public_html/xml/'))
+});
+gulp.task('xsl', function buildHTML() {
+  return gulp.src('assets/xsl/**/*.pug')
+  .pipe(pug({
+    pretty: true
+    // Your options in here.
+  }))
+  .pipe(gulp.dest('public_html/xsl/'))
 });
 
 gulp.task('watch', function(){
   gulp.watch('assets/**/*.pug', ['views']);
 });
 
-gulp.task('default', ['views', 'watch']);
+gulp.task('default', ['xml','xsl','watch']);
