1f797da2abc85034c743f325216b812a98d9fdac
Giovanni Rodighiero <giova.rod93@gmail.com>
2016-08-15 17:37:20 +0200

    added clearform to all

---
+++ public_html/javascript/main.js
@@ -0,0 +1,41 @@
+function renderModal(id, collection) {
+  var div = document.getElementById('modal-div');
+  var button = document.getElementById("modal-submit");
+  var modal = document.getElementById("modal");
+  var input1 = document.createElement("input");
+  input1.type = "hidden";
+  input1.name = "id";
+  input1.value = id;
+  input1.id = "idInfo";
+  modal.insertBefore(input1, button);
+  var input2 = document.createElement("input");
+  input2.type = "hidden";
+  input2.name = "collection";
+  input2.value = collection;
+  input2.id = "collectionInfo"
+  modal.insertBefore(input2, button);
+  div.className = "lightbox";
+  return false;
+}
+function cancelModal(id, collection) {
+  var div = document.getElementById('modal-div');
+  var input1 = document.getElementById('idInfo');
+  var input2 = document.getElementById("collectionInfo");
+  var modal = document.getElementById("modal");
+  modal.removeChild(input1);
+  modal.removeChild(input2);
+
+  div.className = "";
+  return false;
+}
+function clearForm(ids, values) {
+  var idsArray = [];
+  idsArray = ids.split(' ');
+  for (var i = 0; i < idsArray.length; i++) {
+    if(values)
+      document.getElementById(idsArray[i]).value = values[i];
+    else
+      document.getElementById(idsArray[i]).value = '';
+  }
+  return false;
+}
